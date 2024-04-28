556b0b23db189669a17b263f7d288496c7622f03
Sinoxolo-PAL <sinoxolo.cotyi@gmail.com>
2024-04-28 03:09:40 -0700

    initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+/**
+ * main - entry point for the function
+ * return - always returns 0 after function is done executing
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
