541239c35a61a5c5e90523b7034d5a41cf7ba1f0
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-07 03:23:53 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* 
+ * main - A program that determines if the random number is positive or negative
+ * Return: 0 (Success) */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("is zero");
+	}
+	else if ( n > 0)
+	{
+		printf("is positive");
+	}
+	else
+	{
+		printf("negative");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project aims to show the control flows of C programming. Namely, if, else and while.
