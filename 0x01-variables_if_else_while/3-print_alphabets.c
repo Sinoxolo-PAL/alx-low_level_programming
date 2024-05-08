4055ac33e73a74a243b377d5f2865bb2c59c9fdb
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-08 12:52:13 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - rints out lowercase alphabets
+ * Description: The function prints from a to z in a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char alpha = 'a';
+
+while (alpha <= 'Z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
