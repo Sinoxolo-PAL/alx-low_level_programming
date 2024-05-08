fd3ea14b966d1dbde3d840c64e1a1f73d812f0f8
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-08 13:16:20 -0700

    added continue

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,17 @@ int main(void)
 {
 char alpha = 'a';
 
-while (alpha <= 'z' && alpha != 'q' && alpha != 'e')
+while (alpha <= 'z')
 {
 if (alpha != 'e' && alpha != 'q')
 {
 putchar(alpha);
 alpha++;
+}
+else
+{
+alpha++;
+continue;
 }
 }
 putchar('\n');
