1ac3f520b81414ddaacae14657be40cf7081a032
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-10 08:36:36 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The function prints numbers from 0 to 9
+ * Description: The functions putchar is the only one
+ * to be used
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar("%d", i);
+}
+putchar('\n');
+return 0;
+}
