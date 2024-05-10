659dbcafbddeea7e6b0b8f211fe083199655a3e3
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-10 09:43:42 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints hexa decimals
+ * Description: Prints all numbers of base 16
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n')
+return (0);
+}
