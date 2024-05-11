b19e027c2a702d7bcc15d37f828d3571ed4f7d71
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-10 21:57:25 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * Description: A function that prints the
+ * string "_putchar" followed by newline
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Exploring functions in C programming
