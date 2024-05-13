0e1fb06e8dd19ff0d4c2d0bf3d2b3754d9d5f0db
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-13 06:46:44 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabets_10x - Print the alphabets in lowercase
+ * 10 times followed by new line
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+
+for (i = 0; i < 10; i++)
+{
+for (letter = 'a'; letter = 'z'; letter++)
+{
+putchar(letter);
+}
+}
+putchar('\n');
+}
