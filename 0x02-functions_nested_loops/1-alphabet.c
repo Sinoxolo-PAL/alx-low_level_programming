c3720645a934661a318bc249522212b0f3afa928
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-13 06:11:50 -0700

    another betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
 
 /**
  * main - prints the alphabets in lowecase
- * Description: returns 0 once all has been printed
+ * Description - returns 0 once all has been printed
  * Return: 0 (Success)
- *
  */
 
 void print_alphabet(void)
