ea7528635916e3108eae264c8e821a6bd319dc2d
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-30 03:52:53 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+ * _isalpha - Checks if character is an alphabet
+ *
+ * @c: is the character input to check
+ *
+ * Return: 1 for alphabets and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+if isalpha(c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
