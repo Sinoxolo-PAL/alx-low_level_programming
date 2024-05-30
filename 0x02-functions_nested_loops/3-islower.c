9a2474f970662dd7dbc7453c1c468ec289a688d1
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-30 02:51:23 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+ * _islower - Checks if character is in lowercase.
+ *
+ * @c: is the character at hand.
+ *
+ * Return: (0) if not lowercase otherwise (1).
+ */
+
+int _islower(int c)
+{
+if islower(c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
