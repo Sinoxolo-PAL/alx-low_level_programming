5082e52ff7987dc1ddc5cf6957640a7271ef55ad
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-05-30 04:09:57 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * @n: an integer input
+ *
+ * Return: 1 for positive, 0 for 0, and -1 for negative sign
+ */
+
+int print_sign(int n)
+{
+if (n == 0)
+{
+putchar(0);
+return (0);
+}
+else if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+}
