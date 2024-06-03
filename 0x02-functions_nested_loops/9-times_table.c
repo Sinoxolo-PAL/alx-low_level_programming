8b05d0456edaa4d44a55f0444fc42fb1964764a2
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-06-03 06:00:07 -0700

    fixed declaration

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,11 @@
 
 void times_table(void)
 {
-for (int row = 0; row <= 9; row++)
+int row, column;
+
+for (row = 0; row <= 9; row++)
 {
-for (int column = 0; column <= 9; column++)
+for (column = 0; column <= 9; column++)
 {
 _putchar((row * column));
 }
