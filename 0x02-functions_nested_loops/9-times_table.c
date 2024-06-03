93be7166255f25df431b5378a155587b2fa62cde
sinoxolo-pal <sinoxolo.cotyi@gmail.com>
2024-06-03 05:32:49 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+int row, column, cell;
+cell = row * column;
+if (row = 0; row <= 9; row++)
+{
+if (column = 0; column <= 9; column++)
+{
+_putchar(cell);
+}
+}
+}
